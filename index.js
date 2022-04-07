bf207e99be2f43ffbad932107127df3478efdadc
chaenabi <find1086@gmail.com>
2022-04-08 00:09:32 +0900

    Remove js files

--- index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var controller_1 = require("./controller/controller");
-controller_1.storageController.remove();
-controller_1.storageController.clear();
--- service/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-// 개발자가 직접 di를 제어, 필요에 따라 다른 객체를 export
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.storageService = void 0;
-var redis_storage_service_1 = require("./redis-storage-service");
-Object.defineProperty(exports, "storageService", { enumerable: true, get: function () { return redis_storage_service_1.storageService; } });
-//export { storageService } from "./in-memory-storage-service"
